2b34a689ab1ee55df126565854271066b4182ae2
TenguTerror <vegasduard@gmail.com>
2023-10-20 18:54:44 -0600

    made the text turn red if no text in input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,14 +5,19 @@ import './CourseInput.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
+      setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -21,8 +26,15 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label>Course Goal</label>
-        <input type='text' onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          type='text'
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            background: !isValid ? 'salmon' : 'transparent',
+          }}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type='submit'>Add Goal</Button>
     </form>
